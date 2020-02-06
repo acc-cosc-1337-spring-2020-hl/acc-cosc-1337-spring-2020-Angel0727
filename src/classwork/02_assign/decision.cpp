be14a7ee8100772ec9cf5d84e798d6093ae49b63
Angel0727 <angel.tinoco@g.austincc.edu>
2020-02-05 19:03:17 -0600

    my failed attempt at assignment 2

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,9 @@
+#include "decision.h"
 //Write the function code that returns the product of hours and hourly_rate.
+
+
+int gross_pay(int hours, int hourly_pay)
+{
+
+	return hours * hourly_pay ;
+}
--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -1,15 +1,21 @@
 //Write the include statement for decisions.h here
-
+#include "decision.h"
+#include <iostream>
 //Write include for capturing input from keyboard and displaying output to screen
 
 
 //Write namespace using statements for cout and cin
+using std::cout; using std::cin;
 
 int main() 
 {
 	//create a double variable named hours
 	//create a double variable named hourly_rate
+	cout << "Enter the amount of hours worked?";
+	cin = hours;
 
+	cout << "Enter your hourly pay rate?";
+	cin = hourly_pay;
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
 
